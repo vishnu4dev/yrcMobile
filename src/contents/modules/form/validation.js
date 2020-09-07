1c51033b1f4d189597aebb4fa809a8dbcb866d71
vishnu4dev <2vishnukkumar@gmail.com>
2020-09-08 00:20:46 +0530

    feat: yrc starter kit

---
+++ src/contents/modules/form/validation.js
@@ -0,0 +1,19 @@
+import * as EmailValidator from 'email-validator';
+
+export function email(value) {
+  return value && !EmailValidator.validate(value.trim()) ? 'Invalid email' : null;
+}
+
+function isDirty(value) {
+  return value || value === 0;
+}
+
+export function required(requiredFields, values) {
+  return requiredFields.reduce(
+    (fields, field) => ({
+      ...fields,
+      ...(isDirty(values[field]) ? undefined : { [field]: 'Required' }),
+    }),
+    {},
+  );
+}
